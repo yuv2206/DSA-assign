f86fa0743a6aba3f08c7870bacf4491380fba03d
yuv2206 <95854221+yuv2206@users.noreply.github.com>
2025-09-21 17:26:23 +0530

    Add files via upload

---
+++ ass3_4.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int pre(char c) {
+    if (c == '*' || c == '/') {
+        return 2;
+    }
+    if (c == '+' || c == '-') {
+        return 1;
+    }
+    return -1;
+}
+
+int main() {
+    string infix;
+    cout << "Enter the expression: ";
+    cin >> infix;
+
+    string postfix = "";
+    stack<char> s;
+    s.push('(');
+    infix += ')';
+
+    for (char c : infix) {
+        if (c == '(') {
+            s.push(c);
+        } else if (isalnum(c)) {
+            postfix += c;
+        } else if (c == ')') {
+            while (!s.empty() && s.top() != '(') {
+                postfix += s.top();
+                s.pop();
+            }
+            if (!s.empty()) {
+                s.pop();
+            }
+        } else {
+            while (!s.empty() && s.top() != '(' && pre(s.top()) >= pre(c)) {
+                postfix += s.top();
+                s.pop();
+            }
+            s.push(c);
+        }
+    }
+
+    cout << "Postfix: " << postfix << endl;
+
+    return 0;
+}
+
