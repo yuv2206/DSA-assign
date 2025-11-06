5ea1ab335e064d97e429ed26c83d8ff56598d6d8
yuv2206 <95854221+yuv2206@users.noreply.github.com>
2025-11-06 18:25:32 +0530

    Create ques_1.cpp

---
+++ ASSIGN_8/ques_1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+void preorder(Node* root) {
+    if (root == nullptr)
+        return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(Node* root) {
+    if (root == nullptr)
+        return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void postorder(Node* root) {
+    if (root == nullptr)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main() {
+    Node* root = new Node(4);
+    root->left = new Node(2);
+    root->right = new Node(6);
+    root->left->left = new Node(1);
+    root->left->right = new Node(3);
+    root->right->left = new Node(5);
+    root->right->right = new Node(7);
+
+    cout << "Pre-order Traversal: ";
+    preorder(root);
+    cout << endl;
+
+    cout << "In-order Traversal: ";
+    inorder(root);
+    cout << endl;
+
+    cout << "Post-order Traversal: ";
+    postorder(root);
+    cout << endl;
+
+    return 0;
+}
